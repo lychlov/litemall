aea012b1631cb3042339662ab24ca36080310b4a
lychlov <czk499658904@126.com>
2019-12-27 15:37:19 +0800

    changemysql

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -1,8 +1,8 @@
-drop database if exists litemall;
-drop user if exists 'litemall'@'localhost';
+drop database if exists junji_mall;
+drop user if exists 'junji_mall'@'localhost';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
-create database litemall default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database junji_mall default character set utf8mb4 collate utf8mb4_unicode_ci;
 use litemall;
-create user 'litemall'@'localhost' identified by 'litemall123456';
-grant all privileges on litemall.* to 'litemall'@'localhost';
+create user 'junji_mall'@'localhost' identified by 'Lychlov_929';
+grant all privileges on litemall.* to 'junji_mall'@'localhost';
 flush privileges;
