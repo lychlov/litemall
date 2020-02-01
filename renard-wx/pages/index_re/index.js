0322c80bbfdce4266097a3d3ca84c91c8af2b9c5
lychlov <czk4996558904@126.com>
2020-02-02 01:08:53 +0800

    page update

--- renard-wx/pages/index_re/index.js
+++ renard-wx/pages/index_re/index.js
@@ -13,28 +13,39 @@ Page({
     brands: [],
     groupons: [],
     floorGoods: [],
-    banner: [],
-    channel: [],
-    coupon: [],
-    goodsCount: 0
+    // banner: [],
+    channel: []
   },
-
   onShareAppMessage: function() {
     return {
-      title: 'litemall小程序商场',
-      desc: '开源微信小程序商城',
+      title: 'Renard',
+      desc: '唯爱与美食不可辜负',
       path: '/pages/index/index'
     }
   },
 
+  saveFormId: function(v) {
+    if (v.detail.formId != 'the formId is a mock one') {
+      util.request(api.UserFormIdCreate, {
+        formId: v.detail.formId
+      });
+    }
+  },
+
   onPullDownRefresh() {
-    wx.showNavigationBarLoading() //在标题栏中显示加载
     this.getIndexData();
-    wx.hideNavigationBarLoading() //完成停止加载
     wx.stopPullDownRefresh() //停止下拉刷新
   },
 
   getIndexData: function() {
+    wx.showLoading({
+      title: '加载中',
+    });
+
+    setTimeout(function() {
+      wx.hideLoading()
+    }, 2000);
+
     let that = this;
     util.request(api.IndexUrl).then(function(res) {
       if (res.errno === 0) {
@@ -44,24 +55,26 @@ Page({
           topics: res.data.topicList,
           brands: res.data.brandList,
           floorGoods: res.data.floorGoodsList,
-          banner: res.data.banner,
+          // banner: res.data.banner,
           groupons: res.data.grouponList,
-          channel: res.data.channel,
-          coupon: res.data.couponList
+          channel: res.data.channel
         });
+        wx.hideLoading();
       }
-    });
-    util.request(api.GoodsCount).then(function (res) {
-      that.setData({
-        goodsCount: res.data
-      });
     });
   },
   onLoad: function(options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          windowWidth: res.windowWidth,
+          windowHeight: res.windowHeight
+        });
+      }
+    });
 
-    // 页面初始化 options为页面跳转所带来的参数
     if (options.scene) {
-      //这个scene的值存在则证明首页的开启来源于朋友圈分享的图,同时可以通过获取到的goodId的值跳转导航到对应的详情页
       var scene = decodeURIComponent(options.scene);
       console.log("scene:" + scene);
 
@@ -95,7 +108,7 @@ Page({
 
     // 页面初始化 options为页面跳转所带来的参数
     if (options.goodId) {
-      //这个goodId的值存在则证明首页的开启来源于分享,同时可以通过获取到的goodId的值跳转导航到对应的详情页
+      //这个pageId的值存在则证明首页的开启来源于用户点击来首页,同时可以通过获取到的pageId的值跳转导航到对应的详情页
       wx.navigateTo({
         url: '../goods/goods?id=' + options.goodId
       });
@@ -103,7 +116,7 @@ Page({
 
     // 页面初始化 options为页面跳转所带来的参数
     if (options.orderId) {
-      //这个orderId的值存在则证明首页的开启来源于订单模版通知,同时可以通过获取到的pageId的值跳转导航到对应的详情页
+      //这个pageId的值存在则证明首页的开启来源于用户点击来首页,同时可以通过获取到的pageId的值跳转导航到对应的详情页
       wx.navigateTo({
         url: '../ucenter/orderDetail/orderDetail?id=' + options.orderId
       });
@@ -123,25 +136,4 @@ Page({
   onUnload: function() {
     // 页面关闭
   },
-  getCoupon(e) {
-    if (!app.globalData.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    }
-
-    let couponId = e.currentTarget.dataset.index
-    util.request(api.CouponReceive, {
-      couponId: couponId
-    }, 'POST').then(res => {
-      if (res.errno === 0) {
-        wx.showToast({
-          title: "领取成功"
-        })
-      }
-      else{
-        util.showErrorToast(res.errmsg);
-      }
-    })
-  },
-})
+});
